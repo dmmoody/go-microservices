4f381e0be583b19f47855f422d9834cc381821a9
Duane M. Moody <dmmoody@gmail.com>
2023-08-19 17:38:53 -0400

    add product struct

--- internal/models/model_product.go
+++ internal/models/model_product.go
@@ -1,1 +1,9 @@
 package models
+
+type Product struct {
+	ProductId string `gorm:"primaryKey" json:"productId"`
+	Name      string `json:"name"`
+	Price     string `json:"price"`
+	VendorId  string `json:"vendorId"`
+	Vendor    Vendor `gorm:"foreignKey:VendorId" json:"vendor"`
+}
